cba7b412fdd1f29c697d80257b7ba0fa9de9d7f4
MahyParaquett <mahyaraparaquett@gmail.com>
2023-08-28 20:25:16 -0300

    exercicio 1 refeito

--- listas_lp/src/br/com/poo/lista1/Exercicio1.java
+++ listas_lp/src/br/com/poo/lista1/Exercicio1.java
@@ -4,26 +4,37 @@ import java.util.Scanner;
 
 //programa no portugol
 public class Exercicio1 {
-	
+
 //funcao inicio no portugol
 	public static void main(String[] args) {
-		//definir variavel
+		// definir variavel
 		String nome;
-		
-		//importar biblioteca
+		String sobrenome;
+
+		// importar biblioteca
 		Scanner sc = new Scanner(System.in);
-		
-		//funcao escreva no portugol
+
+		// funcao escreva no portugol
 		System.out.println("Por favor, digite seu nome: ");
-		
-		//preenchendo a variável com o que usuario colocar
-		nome = sc.next();
-		
-		//laco de repeticao para limpar a tela
-		for(int i=0; i<20; i++) {
-			System.out.println("\n");
-}
+
+		// preenchendo a variável com o que usuario colocar
+		nome = sc.nextLine();
+
+		// laco de repeticao para limpar a tela
+		for (int i = 0; i < 5; i++) {
+			System.out.println();
+		}
 		System.out.println("Agora, informe seu sobrenome: ");
+		// funcao leia
+		sobrenome = sc.nextLine();
+		sc.close();
+
+		// laco de repeticao para limpar a tela
+		for (int i = 0; i < 5; i++) {
+			System.out.println();
+		}
+		// funcao escreva
+		System.out.println("Seja bem-vindo(a) " + nome + " " + sobrenome);
 	}
 
 }
