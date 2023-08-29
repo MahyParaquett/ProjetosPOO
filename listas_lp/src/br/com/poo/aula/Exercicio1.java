9c99aa7c26d391e2b43969158452b357b5eb6dcc
MahyParaquett <mahyaraparaquett@gmail.com>
2023-08-28 23:43:41 -0300

    exercios do slide

--- listas_lp/src/br/com/poo/aula/Exercicio1.java
+++ listas_lp/src/br/com/poo/aula/Exercicio1.java
@@ -1,10 +1,48 @@
 package br.com.poo.aula;
 
+//lista de bibliotecas
+import java.util.Scanner;
+
+//programa no portugol
 public class Exercicio1 {
 
+//funcao inicio no portugol
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// definir variavel
+		String nome;
+		String sobrenome;
 
+		// intancia
+		Scanner sc = new Scanner(System.in);
+
+		// funcao escreva no portugol
+		System.out.println("Por favor, digite seu nome: ");
+
+		// funcao leia
+		nome = sc.nextLine();
+
+		// laco de repeticao para limpar a tela
+		for (int i = 0; i < 5; i++) {
+			System.out.println();
+		}
+		// funcao escreva no portugol
+		System.out.println("Agora, informe seu sobrenome: ");
+		
+		// funcao leia
+		sobrenome = sc.nextLine();
+
+		// tudo que abre tem que fechar
+		sc.close();
+
+		// funcao para limpar a tela
+		limpa();
+		
+		// funcao escreva
+		System.out.println("Seja bem-vindo(a) " + nome + " " + sobrenome);
 	}
-
+	public static void limpa() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println();
+		}
+	}
 }
--- listas_lp/src/br/com/poo/aula/Exercicio2.java
+++ listas_lp/src/br/com/poo/aula/Exercicio2.java
@@ -1,10 +1,27 @@
 package br.com.poo.aula;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		float numero1;
+		float numero2;
+		float soma;
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		System.out.println("Digite o primeiro numero: ");
+		numero1 = sc.nextFloat();
+		
+		System.out.println("Digite o segundo numero: ");
+		numero2 = sc.nextFloat();
+		
+		soma = numero1 + numero2;
+		sc.close();
+				
+		System.out.println("A soma desses numeros é: "+ soma);
+				
 	}
 
 }
